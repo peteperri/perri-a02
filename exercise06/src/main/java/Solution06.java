459f7db2efe764b455696ada4a585715e47564ef
peteperri <peteperri4@gmail.com>
2021-09-09 22:23:28 -0400

    created ex10 + wrote my name woops lol

--- exercise06/src/main/java/Solution06.java
+++ exercise06/src/main/java/Solution06.java
@@ -1,6 +1,6 @@
 /*
  *  UCF COP3330 Fall 2021 Assignment 2 Solution
- *  Copyright 2021 first_name last_name
+ *  Copyright 2021 Peter Perri
  */
 /*
     Pseudocode:
---
+++ exercise10/src/main/java/Solution10.java
@@ -0,0 +1,10 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 first_name last_name
+ */
+
+public class Solution10 {
+    public static void main(String[] args) {
+
+    }
+}
