d4bfd5e03cef9fd1506d32fa080e3c0012e1eb62
peteperri <peteperri4@gmail.com>
2021-09-09 22:14:54 -0400

    did ex9

---
+++ exercise09/src/main/java/Solution09.java
@@ -0,0 +1,42 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 first_name last_name
+ */
+/*
+Pseudocode:
+write a function to calculate and print how many gallons of paint are needed (PaintCalc)
+write a function to calculate area
+create new scanner object
+create constant variable for a gallon of paint (350 sq feet)
+prompt the user for the length and width of their room
+assign the l and w to variables
+call area function, assign to area variable
+call PaintCalc
+ */
+import java.util.Scanner;
+public class Solution09 {
+
+    static final int CONVERSION_RATE = 350;
+
+    static void paintCalc(double area){
+        double temp = (area/CONVERSION_RATE);
+        double buckets = Math.ceil(temp);
+        System.out.printf("You will need to purchase %f gallons of paint to cover %f square feet.", buckets, area);
+    }
+    static double areaCalc(double length, double width){
+        return length * width;
+    }
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How long is the room?");
+        double length = input.nextFloat();
+        System.out.println("How wide is the room?");
+        double width = input.nextFloat();
+
+        double area = areaCalc(length, width);
+        paintCalc(area);
+
+    }
+}
